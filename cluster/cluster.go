541ec28dc922fcf8bff76017a7c88087444870ed
Daniel Czerwonk <daniel@dan-nrw.de>
2018-01-12 20:13:13 +0100

    Id -> ID

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -1,7 +1,7 @@
 package cluster
 
 type Cluster struct {
-	Id          string `xml:"id,attr"`
+	ID          string `xml:"id,attr"`
 	Name        string `xml:"name"`
 	Description string `xml:"description"`
 }
