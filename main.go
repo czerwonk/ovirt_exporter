c5904abef777454ee9a5f72e44d89ee5d708c402
Daniel Czerwonk <czerwonk@users.noreply.github.com>
2019-06-03 08:49:20 +0200

    Minor release due to dep upgrade

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/prometheus/common/log"
 )
 
-const version string = "0.8.5"
+const version string = "0.8.6"
 
 var (
 	showVersion     = flag.Bool("version", false, "Print version information.")
